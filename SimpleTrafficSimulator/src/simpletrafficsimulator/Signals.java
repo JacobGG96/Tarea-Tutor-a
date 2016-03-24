0a47d594f35050229f812714fdeac3e515b96db3
Ed-Salmeron <eduardote1097@gmail.com>
2016-03-24 13:09:10 -0600

    Added files via upload

---
+++ SimpleTrafficSimulator/src/simpletrafficsimulator/Signals.java
@@ -0,0 +1,24 @@
+package TrafficSimulador;
+
+public class Signals implements IConstants {
+	
+	public void setsignal(boolean _state, String _name){
+		this._state = _state;
+		this._name = _name;
+	}
+	public int is_state() {
+		return _state;
+	}
+
+	public void set_state(boolean _state) {
+		this._state = _state;
+	}
+	public String get_name() {
+		return _name;
+	}
+
+	public void set_name(String _name) {
+		this._name = _name;
+	}
+
+}
