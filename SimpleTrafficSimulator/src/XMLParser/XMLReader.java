5883e435a21ec4735f1c12b9f9bcfd893e75a811
Daniel <Daniel@Daniel_Gonzalez>
2016-04-11 23:08:19 -0600

    Creación del xml de ejemplo  para carreteras

--- SimpleTrafficSimulator/src/XMLParser/XMLReader.java
+++ SimpleTrafficSimulator/src/XMLParser/XMLReader.java
@@ -1,5 +1,6 @@
 package XMLParser;
 
+import Listas.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -12,52 +13,33 @@ public class XMLReader {
     //Se crea un SAXBuilder para poder parsear el archivo
     SAXBuilder builder = new SAXBuilder();
     File xmlFile = new File( "Carreteras.xml" );
+    GenericList lista = new GenericList();  
     
     public void cargarXML(){
     
         try{
             //Se crea el documento a traves del archivo
             Document document = (Document) builder.build(xmlFile);
-
+               
             //Se obtiene la raiz 'tables'
             Element rootNode = document.getRootElement();
 
             //Se obtiene la lista de hijos de la raiz 'tables'
-            List list = rootNode.getChildren( "tabla" ).;
-
+            int tamañolista = rootNode.getChildren( "carretera" ).size();
+            
+            System.out.println(tamañolista);
             //Se recorre la lista de hijos de 'tables'
-            for ( int i = 0; i < list.size(); i++ ){
+            for ( int i = 0; i < tamañolista; i++ ){
                 //Se obtiene el elemento 'tabla'
-                Element tabla = (Element) list.get(i);
+                Element a = (Element) rootNode.getChildren().get(i);
 
                 //Se obtiene el atributo 'nombre' que esta en el tag 'tabla'
-                String nombreTabla = tabla.getAttributeValue("nombre");
-
-                //System.out.println( "Tabla: " + nombreTabla );
-
-                //Se obtiene la lista de hijos del tag 'tabla'
-                List lista_campos = tabla.getChildren();
-
-                System.out.println( "\tNombre\t\tTipo\t\tValor" );
-
-                //Se recorre la lista de campos
-                for ( int j = 0; j < lista_campos.size(); j++ )
-                {
-                    //Se obtiene el elemento 'campo'
-                    Element campo = (Element)lista_campos.get( j );
-
-                    //Se obtienen los valores que estan entre los tags '<campo></campo>'
-                    //Se obtiene el valor que esta entre los tags '<nombre></nombre>'
-                    String nombre = campo.getChildTextTrim("nombre");
-
-                    //Se obtiene el valor que esta entre los tags '<tipo></tipo>'
-                    String tipo = campo.getChildTextTrim("tipo");
-
-                    //Se obtiene el valor que esta entre los tags '<valor></valor>'
-                    String valor = campo.getChildTextTrim("valor");
-
-                    System.out.println( "\t"+nombre+"\t\t"+tipo+"\t\t"+valor);
-                }
+                String nombreCarretera = a.getAttributeValue("nombre");
+                
+                lista.insertarAlFinal(nombreCarretera);
+                
+                lista.imprimir();
+                
             }
         }
 
