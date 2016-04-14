9f3c847b746b172a22b327025c0cc32ace4e10b5
ale5597 <ale5597@gmail.com>
2016-04-14 01:15:37 -0600

    Added files via upload

---
+++ SimpleTrafficSimulator/src/Vehiculo.java
@@ -0,0 +1,38 @@
+package vehiculos;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Vehiculo {
+	private static final int width = 5;
+	private static final int height = 5;
+	private int posX;
+	private int posY;
+	private Ventana ventana;
+
+	public Vehiculo(int posX, int posY){
+		this.posX=posX;
+		this.posY=posY;
+		this.ventana=ventana;
+	}
+
+
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}	
+	
+	public void paint(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillOval(posX,posY,width,height);
+	}
+	
+}
---
+++ SimpleTrafficSimulator/src/Ventana.java
@@ -0,0 +1,37 @@
+package vehiculos;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Ventana extends JPanel{
+	Vehiculo v1 = new Vehiculo(30, 50);
+	//Agregar arreglo de vehiculos
+	
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(Color.black);
+		//Este siguiente es solo para hacer una pruebba
+		v1.paint(g2d);
+		//Hacer un while para pintar los vehiculos tipo:
+		// while lista i != null{
+		//		lista i.paint(g2d)
+	}
+	
+
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Simulator");
+		Ventana ventana = new Ventana();
+		frame.add(ventana);
+		frame.setSize(665, 500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
