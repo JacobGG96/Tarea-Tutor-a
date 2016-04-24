317539cedb9ab36d0142ed32c99f41eab23e5639
Jacob <jacob.gonga@gmail.com>
2016-04-23 19:00:07 -0600

    Creación de la clase MovVehículos que calcula las posiciones del movimiento de un vehículo

---
+++ SimpleTrafficSimulator/src/UI/MovVehiculos.java
@@ -0,0 +1,37 @@
+package UI;
+
+import Listas.QueueArray;
+
+public class MovVehiculos {
+    
+    
+    int sub_hip = 0;
+    
+    private QueueArray nueva_pos(int pActualx, int pActualy, int pNuevox, int pNuevoy,
+            int pVelactual){
+        double cateto1 = Math.abs(pNuevox - pActualx);
+        double cateto2 = Math.abs(pNuevoy - pActualy);
+        double distancia_hip = Math.sqrt(Math.pow(cateto2, 2) + Math.pow(cateto1, 2));
+        double angulo = Math.toDegrees(Math.atan(cateto2 / cateto1)); 
+        sub_hip += convertTo(pVelactual);
+        QueueArray nueva_posicion = new QueueArray(Integer.class, 2);
+        
+        nueva_posicion.enqueue(calc_catetox(sub_hip, angulo));
+        nueva_posicion.enqueue(calc_catetoy(sub_hip, angulo));
+        
+        return nueva_posicion;
+    }
+    
+    private int convertTo(int velocidad){
+        return (velocidad * (1 / 300) * 2);        
+    }
+    
+    private double calc_catetox(int hipotenusa, double angulo){
+        return (hipotenusa * Math.cos(Math.toRadians(angulo)));
+    }
+    
+    private double calc_catetoy(int hipotenusa, double angulo){
+        return (hipotenusa * Math.sin(Math.toRadians(angulo)));
+    }
+    
+}
