6f34785a4cb98acf58e7a94779be91e247606333
Jacob <jacob.gonga@gmail.com>
2016-04-28 17:41:44 -0600

    main

--- SimpleTrafficSimulator/src/UI/GeneraMapa.java
+++ SimpleTrafficSimulator/src/UI/GeneraMapa.java
@@ -1,6 +1,7 @@
 package UI;
 
 import Listas.*;
+import Logic.Highway;
 import XMLParser.XMLReader;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -8,18 +9,53 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
 
+/**
+ * Clase que pinta las lineas y arcos que representan las rectas
+ * y curvas
+ * 
+ */
 public class GeneraMapa extends JPanel{
     private XMLReader _reader;
     private GenericListHw _listaDatos; 
     private GenericList _listaNodosSec;
     
-    
+    /**
+     * Constructor de la clase GeneraMapa
+     */
     public GeneraMapa(){
         _reader = new XMLReader();
-        _listaDatos = _reader.cargarXML();
+        _listaDatos = getListaXML();//_reader.cargarXML();
         _listaNodosSec = new GenericList();
     }
     
+    /**
+     * Crea una lista para usar de prueba 
+     * @return GenericListHw
+     */
+    public GenericListHw getListaXML(){
+        GenericListSec lista = new GenericListSec();
+        GenericListSS listass = new GenericListSS();
+        GenericListSS listass2 = new GenericListSS();
+        GenericListHw carreteras = new GenericListHw();
+        
+        listass.insertarAlFinal("SubSeccion1", 80, 0, 2, 25);
+        listass.insertarAlFinal("SubSeccion2", 90, 1, 3, 25);
+        
+        listass2.insertarAlFinal("SubSeccion3", 80, 0, 2, 25);
+        listass2.insertarAlFinal("SubSeccion4", 90, 1, 3, 25);
+        
+        lista.insertarAlFinal("Recta", 160, 0, listass, 5, 5, 30, 30, 50);
+        lista.insertarAlFinal("Curva", 0, 10, listass2, 6, 6, 31, 31, 50);
+        
+        carreteras.insertarAlFinal("SJ-Cartago", 1000, 2, lista);
+        
+        return carreteras;
+    }
+    
+    /**
+     * Método que parsea los datos de la estructura de datos
+     * que contiene la información del XML.
+     */
     private void parseaDatos() {
         //GenericList listaNodosSec = new GenericList();
         try {
@@ -35,7 +71,10 @@ public class GeneraMapa extends JPanel{
             Logger.getLogger(GeneraMapa.class.getName()).log(Level.SEVERE, null, ex);
         } 
     }
-    
+    /**
+     * Método que pinta las tramos
+     * @param h objeto de la clase Graphics
+     */
     @Override
     public void paintComponent(Graphics h){
         super.paintComponent(h);
