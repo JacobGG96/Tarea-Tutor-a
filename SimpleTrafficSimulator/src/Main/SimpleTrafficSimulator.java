53da222d4c814bc60a1a6d92e34fcedfd0f5fe06
Jacob González Gámez <jacob.gonga@gmail.com>
2016-04-28 17:23:08 -0600

    main

--- SimpleTrafficSimulator/src/Main/SimpleTrafficSimulator.java
+++ SimpleTrafficSimulator/src/Main/SimpleTrafficSimulator.java
@@ -1,9 +1,21 @@
 package Main;
 
+import UI.GeneraMapa;
+import java.awt.Color;
+import javax.swing.JFrame;
+
 @SuppressWarnings({ })
 public class SimpleTrafficSimulator {
 
     public static void main (String args[]){
+        JFrame jf = new JFrame("Mapa");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        GeneraMapa mapa = new GeneraMapa();
+        mapa.setBackground(Color.WHITE);
+        jf.add(mapa);
+        jf.setSize(400, 220);
+        jf.setVisible(true);
         
     }
     
