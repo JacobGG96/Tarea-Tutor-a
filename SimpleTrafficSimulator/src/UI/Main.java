72b60cb8839e08a2d63b1d9023747c767e7a038d
ale5597 <ale5597@gmail.com>
2016-04-28 07:47:27 -0600

    Update Main.java

--- SimpleTrafficSimulator/src/UI/Main.java
+++ SimpleTrafficSimulator/src/UI/Main.java
@@ -10,15 +10,30 @@ import Listas.*;
 
 public class Main extends JPanel{
 	private static final long serialVersionUID = 1L;
-	Vehiculo v1 = new Vehiculo(30, 50);
-	Calle c1= new Calle(50,60,432,333);
+	//Vehiculo v1 = new Vehiculo(30, 50);
+	//Calle c1= new Calle(50,60,432,333);
 	
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(Color.black);
-		v1.paint(g2d);
-		c1.paint(g2d);
+	//	v1.paint(g2d);
+	//	c1.paint(g2d);
+		int cont;
+		String name;
+		String finaleName;
+		cont=0;
+		while (listaHw.getNext()!=null){
+			tmpHw=listaHw.getHead();
+			while (tmpHw.getTramos().getNext!=null){
+				finaleName="c"+ Integer.toString(cont);
+				tmpSec=tmpHw.getTramos();
+				Calle finaleName =new Calle(tmp.getPosXi,tmp.getPosYi,tmp.getPosXf,tmp.getPosYf);
+				finaleName.paint(g2d);
+				cont++;	
+			}
+			tmpHw=temHw.getNext();
+		}
 	}
 	
 	public static void main(String[] args) {
